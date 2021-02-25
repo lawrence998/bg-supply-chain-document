535be3bdf30ee986ba8c42e3dd30034c257ffb94
lichihang <lichihang@banggood.com>
2021-02-25 14:06:17 +0800

    Updates

--- assets/js/59.e9dadd24.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[59],{504:function(n,w,o){}}]);
---
+++ assets/js/83.9200955e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[83],{520:function(n,w,o){}}]);
